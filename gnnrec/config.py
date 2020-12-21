f79cd835aebb8802f3bebcf0a2635b8c6de8623b
ZZy979 <979481894@qq.com>
2020-12-21 20:56:56 +0800

    创建目录结构

---
+++ gnnrec/config.py
@@ -0,0 +1,7 @@
+import os
+
+# 项目根目录
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# 数据集目录
+DATA_DIR = os.path.join(BASE_DIR, 'data')
---
+++ gnnrec/hgrl/run.py
@@ -0,0 +1,6 @@
+from gnnrec.config import BASE_DIR, DATA_DIR
+
+if __name__ == '__main__':
+    print('Hello, world!')
+    print(BASE_DIR)
+    print(DATA_DIR)
